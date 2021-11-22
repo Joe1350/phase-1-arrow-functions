e8b2914c39dc8b8a3179d05c390e9edd8a8f6a34
Joseph Martin <josephmartin1350@gmail.com>
2021-11-22 13:03:06 -0700

    commit tree c85d8e1ac30ad0b91db468ba37dfdf7feda9d13a

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(num1 = 2000, num2 = 100) {
+    return num1 / num2;
+}
+
+// const square = n => Math.pow(n, 2);
+
+const square = n => n * n;
+
+const add = (num1, num2) => num1 + num2;
